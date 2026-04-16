6a8b01656d34e2e657e524080d3ee5c45b0ab32b
baihaqiramadhani-11 <baihakiramadhani66@gmail.com>
2026-04-16 18:02:07 +0700

    data10

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -104,3 +104,12 @@ void binarySearch()
         cin >> ulang;
 
     } while (ulang == 'y' || ulang == 'Y');
+
+}    
+int main()
+{
+    input();
+    bubbleSortArray();
+    display;
+    binarySearch();
+}
